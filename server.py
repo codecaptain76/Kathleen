9869b98108b61c99781a5cdb7a3742f0fc4e8084
keirrahall <codecaptain76@gmail.com>
2017-02-22 17:03:30 -0800

    working on background, contact form, approach in  progress

---
+++ server.py
@@ -0,0 +1,47 @@
+from jinja2 import StrictUndefined
+
+from flask import Flask, render_template, redirect, request, flash, session as browser_session
+
+from secrets import amm_chain, all_chain
+import pprint
+
+import requests
+
+import json
+
+
+
+
+
+
+
+
+
+
+@app.route('/')
+def home():
+	return render_template("home.html")
+
+
+@app.route('/work')
+def work():
+	return render_template("work.html")
+
+
+@app.route('/impact')
+def impact():
+	return render_template("impact.html")
+
+
+@app.route('/approach')
+def approach():
+	return render_template("approach.html")
+
+@app.route('/about')
+def about():
+	return render_template("about.html")
+
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
+	
