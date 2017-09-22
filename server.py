8f282d8009f8f733ffc22492b17b547eac8cf2eb
Keirra Hall <codecaptain76@users.noreply.github.com>
2017-09-21 21:11:43 -0700

    email app route update

--- server.py
+++ server.py
@@ -1,37 +1,23 @@
 from jinja2 import StrictUndefined
-
 from flask import Flask, render_template, redirect, request, flash, session as browser_session
-
 from secrets import amm_chain, all_chain
 import pprint
-
 import requests
-
 import json
 
-
-
-
-
-
-
-
-
+app = Flask(__name__)
 
 @app.route('/')
 def home():
 	return render_template("home.html")
 
-
 @app.route('/work')
 def work():
 	return render_template("work.html")
 
-
 @app.route('/impact')
 def impact():
 	return render_template("impact.html")
-
 
 @app.route('/approach')
 def approach():
@@ -44,4 +30,8 @@ def about():
 @app.route('/contact')
 def contact():
 	return render_template("contact.html")
+
+@app.route('/send-email', methods= ['POST'])
+def email():
+	email = request.form.get("email")
 	
