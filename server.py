6d52316c109814db1afeeee63d4605079e6597fc
keirrahall <codecaptain76@gmail.com>
2017-11-06 20:02:50 -0800

    repaired picture glitch

--- server.py
+++ server.py
@@ -27,7 +27,9 @@ def approach():
 def about():
 	return render_template("about.html")
 
-
+@app.route('/contact')
+def contact():
+	return render_template("contact.html")
 
 @app.route('/contact', methods= ['POST'])
 def email():
